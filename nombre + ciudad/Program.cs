f60c4bbbe3188388dbd09f9a731f2f2b017fca17
aLlira13 <jose.lirahernandez@cesunbc.edu.mx>
2022-01-14 07:47:49 -0800

    se subio proyecto

---
+++ nombre + ciudad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nombre___ciudad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe una ciudad ");
+            string ciudad = Console.ReadLine();
+
+            Console.WriteLine("Hola Tu nombre es " + nombre + " Bienvenido a " + ciudad);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
